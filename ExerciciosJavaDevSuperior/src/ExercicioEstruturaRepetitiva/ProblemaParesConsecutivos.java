78269a875c8633e6a69109c059bfc078c4480ecc
Dalfior <aecio.dalfior@gmail.com>
2023-06-12 15:45:40 -0300

    Atualização exercício sequencial
    Atualização exercício sequencial

--- ExerciciosJavaDevSuperior/src/ExercicioEstruturaRepetitiva/ProblemaParesConsecutivos.java
+++ ExerciciosJavaDevSuperior/src/ExercicioEstruturaRepetitiva/ProblemaParesConsecutivos.java
@@ -4,27 +4,41 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ProblemaParesConsecutivos {
-	
+
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
-		int x, soma;
-		
-		while(x != 0) {
-		System.out.println("Digite um número inteiro: ");
+
+		int x, incremento = 0, soma = 0;
+
+		System.out.print("Digite um número inteiro: ");
 		x = sc.nextInt();
+
+		while (incremento < 5 && x > 0) {
+			incremento++;
+
+			if (x % 2 == 0) {				
+				soma = ((x * 5) + 20);
+
+
+
+			}
+			else if(x % 2 != 0) {
+				soma = (((x + 1) * 5) + 20);
+
+			}
 		
-		soma = x
-		
-		
+			
+			
+			System.out.println("SOMA " + soma);
+			
+			System.out.print("Digite um número inteiro: ");
+			x = sc.nextInt();
+
+
 		}
-		
-		
-		
-		
+
 		sc.close();
 	}
 
