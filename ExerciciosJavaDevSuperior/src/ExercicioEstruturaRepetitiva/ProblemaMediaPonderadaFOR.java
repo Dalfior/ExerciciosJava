21d4a6d064afbf86ddc4648a0330607aca28179a
Dalfior <aecio.dalfior@gmail.com>
2023-06-16 15:35:14 -0300

    Atualização de exercícios
    Atualização de exercícios

--- ExerciciosJavaDevSuperior/src/ExercicioEstruturaRepetitiva/ProblemaMediaPonderadaFOR.java
+++ ExerciciosJavaDevSuperior/src/ExercicioEstruturaRepetitiva/ProblemaMediaPonderadaFOR.java
@@ -10,10 +10,27 @@ public class ProblemaMediaPonderadaFOR {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		double n, numero1 = 0, numero2 = 0, numero3 = 0, calculo, peso1, peso2, peso3, totalPeso;
 		
+		System.out.print("Quantos casos você vai digitar? ");
+		n = sc.nextInt();
 		
-		
-		
+		for(int i=0; i<n; i++) {
+			
+			System.out.printf("Digite %.0f números: ", n);
+			//System.out.printf("Digite três números: ");
+			numero1 = sc.nextDouble();
+			numero2 = sc.nextDouble();
+			numero3 = sc.nextDouble();
+			
+			peso1 = 2;
+			peso2 = 3;
+			peso3 = 5;
+			totalPeso = peso1 + peso2 + peso3;
+			calculo = ((numero1 * peso1) + (numero2 * peso2) + (numero3 * peso3)) / totalPeso; 
+			System.out.printf("MEDIA = %.1f%n", calculo);
+		}
+
 		
 		sc.close();
 	}
